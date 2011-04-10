3d4d60226021a60766ff74c2f2434fbaf859756b
Dmitriy Vilkov <dav.daemon@gmail.com>
2011-04-10 19:58:20 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/trunk@23 18ec6afb-3d25-4488-acc2-f73458515831

---
+++ qfm/src/mainwindow/foldersview/widgets/tasksview.cpp
@@ -0,0 +1,6 @@
+#include "tasksview.h"
+
+
+TasksView::TasksView(QWidget *parent) :
+	QWidget(parent)
+{}
---
+++ qfm/src/mainwindow/foldersview/widgets/tasksview.h
@@ -0,0 +1,13 @@
+#ifndef TASKSVIEW_H_
+#define TASKSVIEW_H_
+
+#include <QtGui/QWidget>
+
+
+class TasksView : public QWidget
+{
+public:
+	TasksView(QWidget *parent = 0);
+};
+
+#endif /* TASKSVIEW_H_ */

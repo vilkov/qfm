fe2b62b495d848d62b4e8a9ea61dad717e980a1e
Dmitriy Vilkov <dav.daemon@gmail.com>
2011-04-19 14:34:33 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/trunk/qfm@38 18ec6afb-3d25-4488-acc2-f73458515831

---
+++ src/mainwindow/foldersview/filesystemmodel/tasks/perform/performcopytreetask.cpp
@@ -0,0 +1,96 @@
+#include "performcopytreetask.h"
+#include "../../../../../application.h"
+#include <QtCore/QFile>
+
+
+PerformCopyTreeTask::PerformCopyTreeTask(Params *params) :
+	PerformCopyEntryTask(params)
+{
+	Q_ASSERT(params->subtree != 0);
+}
+
+void PerformCopyTreeTask::run(const volatile bool &stopedFlag)
+{
+	Event::EventType type;
+	QDir dir(parameters()->destination.fileSystemTree->fileInfo().absoluteFilePath());
+
+	if (dir.exists() && cd(dir, parameters()->source.entry, stopedFlag))
+	{
+		copy(dir, parameters()->subtree, stopedFlag);
+		type = m_canceled ? Event::CopyFilesCanceled : Event::CopyFilesComplete;
+	}
+	else
+		type = Event::CopyFilesCanceled;
+
+//	QScopedPointer<Event> event(new Event(type));
+//	event->params().fileSystemTree = parameters()->fileSystemTree;
+//	event->params().size = parameters()->size;
+//	event->params().entry = parameters()->entry;
+//	event->params().subtree = parameters()->subtree;
+//	event->params().destination = parameters()->destination;
+//	event->params().destinationDirectory = parameters()->destinationDirectory;
+//	Application::postEvent(parameters()->receiver, event.take());
+}
+
+bool PerformCopyTreeTask::cd(QDir &destination, FileSystemEntry *entry, const volatile bool &stopedFlag)
+{
+	if (destination.cd(entry->fileInfo().fileName()))
+		return true;
+	else
+	{
+		bool tryAgain;
+
+		do
+			if (destination.mkdir(entry->fileInfo().fileName()))
+				if (destination.cd(entry->fileInfo().fileName()))
+					return true;
+				else
+					if (m_skipAllIfNotCopy)
+						break;
+					else
+						askForSkipAllIfNotCopy(
+								entry->lockReason(),
+								tr("Failed to create directory \"%1\" in \"%2\". Skip it?").
+									arg(entry->fileInfo().fileName()).
+									arg(destination.absolutePath()),
+								tryAgain = false,
+								stopedFlag);
+			else
+				if (m_skipAllIfNotCopy)
+					break;
+				else
+					askForSkipAllIfNotCopy(
+							entry->lockReason(),
+							tr("Failed to create directory \"%1\" in \"%2\". Skip it?").
+								arg(entry->fileInfo().fileName()).
+								arg(destination.absolutePath()),
+							tryAgain = false,
+							stopedFlag);
+		while (tryAgain && !stopedFlag && !isControllerDead() && !m_canceled);
+	}
+
+	return false;
+}
+
+void PerformCopyTreeTask::copy(QDir &destination, FileSystemTree *tree, const volatile bool &stopedFlag)
+{
+	bool tryAgain;
+	FileSystemEntry *entry;
+
+	for (FileSystemTree::size_type i = 1, size = tree->size(); i < size && !stopedFlag && !isControllerDead() && !m_canceled; ++i)
+		if ((entry = static_cast<FileSystemEntry*>(tree->child(i)))->fileInfo().isDir())
+		{
+			if (cd(destination, entry, stopedFlag))
+			{
+				copy(destination, static_cast<FileSystemTree*>(tree->subtree(entry)), stopedFlag);
+				destination.cdUp();
+			}
+		}
+		else
+		{
+			copyFile(destination, entry, tryAgain = false, stopedFlag);
+
+			if (tryAgain)
+				--i;
+		}
+}
---
+++ src/mainwindow/foldersview/filesystemmodel/tasks/perform/performcopytreetask.h
@@ -0,0 +1,52 @@
+#ifndef PERFORMCOPYTREETASK_H_
+#define PERFORMCOPYTREETASK_H_
+
+#include "performcopyentrytask.h"
+#include "../scan/scanfilestask.h"
+
+
+class PerformCopyTreeTask : public PerformCopyEntryTask
+{
+	Q_DECLARE_TR_FUNCTIONS(PerformCopyTreeTask)
+
+public:
+	struct Params : public PerformCopyEntryTask::Params
+	{
+		Params(QObject *receiver, const ScanFilesForCopyTask::EventParams &params) :
+			subtree(params.subtree)
+		{
+			source.object = receiver;
+			source.fileSystemTree = params.snapshot.fileSystemTree;
+			source.entry = params.snapshot.entry;
+			destination = params.destination;
+		}
+
+		FileSystemTree *subtree;
+	};
+	struct EventParams : public PerformCopyEntryTask::EventParams
+	{
+		FileSystemTree *subtree;
+	};
+	typedef FileSystemModelEventTemplate<EventParams> Event;
+
+
+public:
+	PerformCopyTreeTask(Params *params);
+
+	virtual void run(const volatile bool &stopedFlag);
+
+protected:
+	inline Params *parameters() const { return static_cast<Params*>(PerformCopyEntryTask::parameters()); }
+
+private:
+	bool cd(QDir &destination, FileSystemEntry *entry, const volatile bool &stopedFlag);
+	void copy(QDir &destination, FileSystemTree *tree, const volatile bool &stopedFlag);
+
+private:
+	bool m_skipAllIfNotCreate;
+	bool m_skipAllIfNotCopy;
+	bool m_overwriteAll;
+	bool m_canceled;
+};
+
+#endif /* PERFORMCOPYTREETASK_H_ */

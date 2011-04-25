4ffde1eed34e68a5a4d1181ff56c9d9c6f23ea1a
Dmitriy Vilkov <dav.daemon@gmail.com>
2011-04-25 15:26:32 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/trunk@48 18ec6afb-3d25-4488-acc2-f73458515831

--- qfm/src/mainwindow/foldersview/filesystemmodel/contextmenu/contextmenu.h
+++ qfm/src/mainwindow/foldersview/filesystemmodel/contextmenu/contextmenu.h
@@ -1,6 +1,7 @@
 #ifndef CONTEXTMENU_H_
 #define CONTEXTMENU_H_
 
+#include <QtCore/qt_windows.h>
 #include <QtGui/QMenu>
 #include "../filesysteminfo.h"
 
@@ -8,14 +9,33 @@
 class ContextMenu
 {
 public:
+	class ContextMenuImp : public QSharedData
+	{
+		Q_DISABLE_COPY(ContextMenuImp)
+
+	public:
+		ContextMenuImp() :
+			QSharedData()
+		{}
+
+		QMenu menu;
+	};
+
+public:
 	ContextMenu();
 	ContextMenu(const FileSystemInfo &info);
-	~ContextMenu();
 
     void popup();
 
 private:
-	QMenu *m_menu;
+    enum
+    {
+    	BufferSize = 1024
+    };
+    void populateMenu(QMenu *menu, HMENU hmenu, MENUITEMINFO &info, char *buffer);
+
+private:
+    QExplicitlySharedDataPointer<ContextMenuImp> m_data;
 };
 
 #endif /* CONTEXTMENU_H_ */

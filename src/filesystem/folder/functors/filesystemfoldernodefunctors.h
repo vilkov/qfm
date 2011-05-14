72f405e39e14d919e7ec4aa67360200e920d52d1
Dmitriy Vilkov <dav.daemon@gmail.com>
2011-05-14 20:02:14 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@92 18ec6afb-3d25-4488-acc2-f73458515831

---
+++ src/filesystem/folder/functors/filesystemfoldernodefunctors.h
@@ -0,0 +1,79 @@
+#ifndef FILESYSTEMFOLDERNODEFUNCTORS_H_
+#define FILESYSTEMFOLDERNODEFUNCTORS_H_
+
+#include "../items/filesystemfoldernodeitem.h"
+
+
+/**
+ *  Closed namespace "Functors"
+ */
+struct Functors
+{
+	class Functor
+	{
+	public:
+		typedef FileSystem::FolderNodeItem Entry;
+
+	public:
+		virtual ~Functor() {}
+
+		inline void operator()(Entry *entry) const { call(entry); }
+
+	protected:
+		virtual void call(Entry *entry) const = 0;
+	};
+
+
+	/***************************************************************************************************/
+	template <typename T>
+	class Callable : public Functor
+	{
+	public:
+		typedef void (T::*Method)(Entry *);
+
+	public:
+		Callable(T *object, Method method) :
+			m_object(object),
+			m_method(method)
+		{}
+
+	protected:
+		virtual void call(Entry *entry) const { (m_object->*m_method)(entry); }
+
+	private:
+		T *m_object;
+		Method m_method;
+	};
+	template <typename T>
+	inline static Callable<T> callTo(T *object, typename Callable<T>::Method method)
+	{ return Callable<T>(object, method); }
+
+
+	/***************************************************************************************************/
+	template <typename T, typename Arg1>
+	class Callable1 : public Functor
+	{
+	public:
+		typedef void (T::*Method)(Entry *, Arg1);
+
+	public:
+		Callable1(T *object, Method method, Arg1 arg1) :
+			m_object(object),
+			m_method(method),
+			m_arg1(arg1)
+		{}
+
+	protected:
+		virtual void call(Entry *entry) const { (m_object->*m_method)(entry, m_arg1); }
+
+	private:
+		T *m_object;
+		Method m_method;
+		Arg1 m_arg1;
+	};
+	template <typename T, typename Arg1>
+	inline static Callable1<T, Arg1> callTo(T *object, typename Callable1<T, Arg1>::Method method, Arg1 arg1)
+	{ return Callable1<T, Arg1>(object, method, arg1); }
+};
+
+#endif /* FILESYSTEMFOLDERNODEFUNCTORS_H_ */

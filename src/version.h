48cf3ab21d0b846149a027f8db3ebaa36cedc540
Dmitriy Vilkov <dav.daemon@gmail.com>
2011-07-20 10:21:43 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@183 18ec6afb-3d25-4488-acc2-f73458515831

--- src/version.h
+++ src/version.h
@@ -1,14 +1,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define MAJOR_VERSION_NUMBER    1
+#define MAJOR_VERSION_NUMBER    0
 #define MINOR_VERSION_NUMBER    0
 #define RELEASE_VERSION_NUMBER  0
 #define BUILD_VERSION_NUMBER    1
 #ifdef RC_INVOKED
-#	define VERSION_STRING       "1.0.0.1\0"
+#	define VERSION_STRING       "0.0.0.1\0"
 #else
-#	define VERSION_STRING       "1.0.0.1"
+#	define VERSION_STRING       "0.0.0.1"
 #endif
 
 

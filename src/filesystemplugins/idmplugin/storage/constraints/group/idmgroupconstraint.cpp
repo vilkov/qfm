0b665f463cbd3dffec39fd6738a05e69abce0a24
Dmitriy Vilkov <dav.daemon@gmail.com>
2011-10-03 14:56:53 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@319 18ec6afb-3d25-4488-acc2-f73458515831

--- src/filesystemplugins/idmplugin/storage/constraints/group/idmgroupconstraint.cpp
+++ src/filesystemplugins/idmplugin/storage/constraints/group/idmgroupconstraint.cpp
@@ -17,4 +17,24 @@ QString GroupConstraint::toString() const
 	return QString();
 }
 
+void GroupConstraint::add(BaseConstraint *constraint)
+{
+	m_items.push_back(constraint);
+}
+
+void GroupConstraint::insert(size_type index, BaseConstraint *constraint)
+{
+	m_items.insert(index, constraint);
+}
+
+void GroupConstraint::swap(size_type index1, size_type index2)
+{
+	m_items.swap(index1, index2);
+}
+
+void GroupConstraint::remove(size_type index)
+{
+	m_items.removeAt(index);
+}
+
 IDM_PLUGIN_NS_END
--- src/filesystemplugins/idmplugin/storage/constraints/group/idmgroupconstraint.h
+++ src/filesystemplugins/idmplugin/storage/constraints/group/idmgroupconstraint.h
@@ -31,6 +31,11 @@ public:
 	BaseConstraint *at(size_type index) const { return m_items.at(index); }
 	size_type size() const { return m_items.size(); }
 
+	void add(BaseConstraint *constraint);
+	void insert(size_type index, BaseConstraint *constraint);
+	void swap(size_type index1, size_type index2);
+	void remove(size_type index);
+
 protected:
 	Type m_type;
 	Container m_items;

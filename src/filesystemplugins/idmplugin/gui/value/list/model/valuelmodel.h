1477e887ad6c81cea6cf6352b417b01b05c4413b
Dmitriy Vilkov <dav.daemon@gmail.com>
2011-10-04 15:31:04 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@330 18ec6afb-3d25-4488-acc2-f73458515831

---
+++ src/filesystemplugins/idmplugin/gui/value/list/model/valuelmodel.h
@@ -0,0 +1,51 @@
+#ifndef VALUEMODEL_H_
+#define VALUEMODEL_H_
+
+#include <QtCore/QList>
+#include <QtCore/QAbstractItemModel>
+#include "../../../../../storage/values/idmvaluereader.h"
+
+
+IDM_PLUGIN_NS_BEGIN
+
+class ValueModel : public QAbstractItemModel
+{
+public:
+	typedef QList<IdmEntityValue*> List;
+	typedef List::size_type        size_type;
+
+public:
+	ValueModel(const IdmContainer &container, const Select &query, QObject *parent = 0);
+	virtual ~ValueModel();
+
+    /* QAbstractItemModel */
+    virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+	virtual int columnCount(const QModelIndex &parent) const;
+	virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+	virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+	virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+    virtual void fetchMore(const QModelIndex &parent);
+    virtual bool canFetchMore(const QModelIndex &parent = QModelIndex()) const;
+	virtual QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
+	virtual QModelIndex parent(const QModelIndex &child) const;
+
+	bool isValid() const { return m_reader.isValid(); }
+	const QString &lastError() const { return m_reader.lastError(); }
+
+	IdmEntityValue *take(const QModelIndex &index);
+
+protected:
+    enum { PrefetchLimit = 256 };
+
+protected:
+    void add(const List &list);
+	void remove(const QModelIndex &index);
+
+protected:
+	IdmValueReader m_reader;
+	List m_items;
+};
+
+IDM_PLUGIN_NS_END
+
+#endif /* VALUEMODEL_H_ */

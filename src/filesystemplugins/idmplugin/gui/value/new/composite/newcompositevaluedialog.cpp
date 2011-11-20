e0be2dc9582f34c3731c5b4da88e7365b39bc3ab
Dmitriy Vilkov <dav.daemon@gmail.com>
2011-11-20 15:07:52 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@422 18ec6afb-3d25-4488-acc2-f73458515831

--- src/filesystemplugins/idmplugin/gui/value/new/composite/newcompositevaluedialog.cpp
+++ src/filesystemplugins/idmplugin/gui/value/new/composite/newcompositevaluedialog.cpp
@@ -1,42 +1,8 @@
 #include "newcompositevaluedialog.h"
-#include "../../model/items/compositevaluevalueitem.h"
-#include "../../model/items/compositevaluepropertyitem.h"
-#include "../../../../storage/values/idmvaluereader.h"
-#include "../../../../../../tools/pointers/pscopedpointer.h"
 
 
 NewCompositeValueDialog::NewCompositeValueDialog(const IdmContainer &container, IdmCompositeEntityValue *value, QWidget *parent) :
 	CompositeValueDialog(container, value, parent)
 {
-	setWindowTitle(tr("New value for \"%1\"").arg(m_entity->name()));
+	setWindowTitle(tr("New value for \"%1\"").arg(value->entity()->name()));
 }
-
-//IdmEntityValue *NewCompositeValueDialog::value()
-//{
-//	IdmContainer::id_type id = container().addValue(m_entity);
-//
-//	if (id != IdmContainer::InvalidId)
-//	{
-//		IdmEntityValue *tmp;
-//		IdmContainer::IdsMap res;
-//		CompositeValuePropertyItem *property;
-//		PScopedPointer<IdmEntityValue> value(IdmValueReader::createValue(m_entity, id));
-//
-//		for (CompositeValueModel::size_type i = 0, size = model().size(); i < size; ++i)
-//		{
-//			IdmContainer::IdsList &list = res[(property = static_cast<CompositeValuePropertyItem*>(model().at(i)))->entity()];
-//			list.reserve(property->size());
-//
-//			for (CompositeValueModel::size_type q = 0, size = property->size(); q < size; ++q)
-//			{
-//				IdmValueReader::addValue(value.data(), tmp = static_cast<CompositeValueValueItem*>(property->at(q))->take());
-//				list.push_back(tmp->id());
-//			}
-//		}
-//
-//		if (container().addValue(m_entity, id, res))
-//			return value.take();
-//	}
-//
-//	return 0;
-//}
--- src/filesystemplugins/idmplugin/gui/value/new/composite/newcompositevaluedialog.h
+++ src/filesystemplugins/idmplugin/gui/value/new/composite/newcompositevaluedialog.h
@@ -10,11 +10,6 @@ class NewCompositeValueDialog : public CompositeValueDialog
 
 public:
 	NewCompositeValueDialog(const IdmContainer &container, IdmCompositeEntityValue *value, QWidget *parent = 0);
-
-//    IdmEntityValue *value();
-
-private:
-	IdmEntity *m_entity;
 };
 
 #endif /* NEWCOMPOSITEVALUEDIALOG_H_ */

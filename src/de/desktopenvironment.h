2197d245a39898f2b14c1497168f2b515c3b53e9
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-01-02 11:25:48 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@485 18ec6afb-3d25-4488-acc2-f73458515831

---
+++ src/de/desktopenvironment.h
@@ -0,0 +1,32 @@
+#ifndef DESKTOPENVIRONMENT_H_
+#define DESKTOPENVIRONMENT_H_
+
+#include <QtCore/QString>
+
+
+class DesktopEnvironment
+{
+public:
+	enum Type
+	{
+		DE_Cde,
+		DE_Kde,
+		DE_4Dwm,
+		DE_Gnome,
+		DE_Unknown
+	};
+
+public:
+	DesktopEnvironment();
+	~DesktopEnvironment();
+
+	Type type() const { return m_type; }
+	bool info(const QString &absoluteFilePath) const;
+
+private:
+	Type m_type;
+	int m_version;
+};
+
+
+#endif /* DESKTOPENVIRONMENT_H_ */
--- src/filesystem/filetype/filetypemanager.cpp
+++ src/filesystem/filetype/filetypemanager.cpp
@@ -1,8 +1,5 @@
 #include "filetypemanager.h"
-#include "xdgmime/src/xdgmime.h"
-#include <QtCore/QDebug>
-#include <QtCore/QFile>
-
+#include "../../de/desktopenvironment.h"
 #ifndef Q_OS_WIN
 #	include <unistd.h>
 #endif
@@ -26,9 +23,7 @@ FileTypeManager::FileTypeManager() :
 {}
 
 FileTypeManager::~FileTypeManager()
-{
-	xdg_mime_shutdown();
-}
+{}
 
 FileTypeManager::MimeType FileTypeManager::type(const QString &absoluteFilePath) const
 {
@@ -37,54 +32,7 @@ FileTypeManager::MimeType FileTypeManager::type(const QString &absoluteFilePath)
 
 Info FileTypeManager::info(const QString &absoluteFilePath) const
 {
-	QByteArray fileName = absoluteFilePath.toUtf8();
-	const char *mimeType = xdg_mime_get_mime_type_from_file_name(fileName.data());
-
-	if (mimeType == XDG_MIME_TYPE_UNKNOWN)
-	{
-		struct stat st;
-		stat(fileName.data(), &st);
-
-		if ((mimeType = xdg_mime_get_mime_type_for_file(fileName.data(), &st)) != XDG_MIME_TYPE_UNKNOWN)
-		{
-			if (const XdgAppArray *apps = xdg_mime_user_apps_lookup(mimeType))
-			{
-				const XdgAppGroup *group;
-				const XdgEntryValueArray *values;
-
-				for (int i = 0, size = xdg_mime_app_array_size(apps); i < size; ++i)
-					if (group = xdg_mime_app_group_lookup(xdg_mime_app_array_item_at(apps, i), "Desktop Entry"))
-						if (values = xdg_mime_app_entry_lookup(group, "Name"))
-							for (int i = 0, size = xdg_mime_entry_value_array_size(values); i < size; ++i)
-								qDebug() << xdg_mime_entry_value_array_item_at(values, i);
-			}
-
-			if (const XdgAppArray *apps = xdg_mime_default_apps_lookup(mimeType))
-			{
-				const XdgAppGroup *group;
-				const XdgEntryValueArray *values;
-
-				for (int i = 0, size = xdg_mime_app_array_size(apps); i < size; ++i)
-					if (group = xdg_mime_app_group_lookup(xdg_mime_app_array_item_at(apps, i), "Desktop Entry"))
-						if (values = xdg_mime_app_entry_lookup(group, "Name"))
-							for (int i = 0, size = xdg_mime_entry_value_array_size(values); i < size; ++i)
-								qDebug() << xdg_mime_entry_value_array_item_at(values, i);
-			}
-
-			if (const XdgAppArray *apps = xdg_mime_known_apps_lookup(mimeType))
-			{
-				const XdgAppGroup *group;
-				const XdgEntryValueArray *values;
-
-				for (int i = 0, size = xdg_mime_app_array_size(apps); i < size; ++i)
-					if (group = xdg_mime_app_group_lookup(xdg_mime_app_array_item_at(apps, i), "Desktop Entry"))
-						if (values = xdg_mime_app_entry_lookup(group, "Name"))
-							for (int i = 0, size = xdg_mime_entry_value_array_size(values); i < size; ++i)
-								qDebug() << xdg_mime_entry_value_array_item_at(values, i);
-			}
-		}
-	}
-
+	DesktopEnvironment().info(absoluteFilePath);
 	return Info();
 }
 

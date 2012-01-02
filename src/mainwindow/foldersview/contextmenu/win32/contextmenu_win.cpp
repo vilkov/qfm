6181dc5902752ac120dfadb26635605a72b967d9
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-01-02 14:54:54 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@487 18ec6afb-3d25-4488-acc2-f73458515831

--- src/mainwindow/foldersview/contextmenu/win32/contextmenu_win.cpp
+++ src/mainwindow/foldersview/contextmenu/win32/contextmenu_win.cpp
@@ -1,1 +1,217 @@
-Unexpected error.  File contents could not be restored from local history during undo/redo.
+#include "contextmenu_win.h"
+//#include "../../../../application.h"
+#include <QtCore/QDir>
+#include <QtGui/QCursor>
+#include <QtGui/QPixmap>
+#include <QtGui/QIcon>
+#include <QMessageBox>
+
+
+//static ContextMenuWin *instance;
+//static Application::WinEventFilter oldFilter;
+
+ContextMenuWin::ContextMenuWin(QWidget *parent) :
+	ContextMenu::Implementation(parent),
+	m_desktop(),
+	m_menu2(),
+	m_menu3()
+{
+//	instance = this;
+
+	if (m_init.is_initialised())
+	{
+		IShellFolder *iFolder;
+
+		if (SUCCEEDED(SHGetDesktopFolder(&iFolder)))
+			m_desktop.set(iFolder, false);
+	}
+}
+
+void ContextMenuWin::popup(const QString &parentDir, const QStringList &files, const QPoint &pos)
+{
+	if (m_init.is_initialised() && !m_desktop.empty())
+	{
+		HRESULT res;
+		ITEMIDLIST *pidl;
+		ItemIdListStorage storage(files.size());
+
+		for (QStringList::size_type i = 0, size = files.size(); i < size; ++i)
+			if (SUCCEEDED(m_desktop->ParseDisplayName(NULL, NULL, (wchar_t*)QDir::toNativeSeparators(files.at(i)).utf16(), NULL, &pidl, NULL)))
+				storage.push_back(pidl);
+
+		if (!storage.isEmpty() &&
+			SUCCEEDED(m_desktop->ParseDisplayName(NULL, NULL, (wchar_t*)QDir::toNativeSeparators(parentDir).utf16(), NULL, &pidl, NULL)))
+		{
+			IShellFolder *iFolder;
+
+			if (SUCCEEDED(m_desktop->BindToObject(pidl, NULL, IID_IShellFolder, (void**)&iFolder)))
+			{
+				stlsoft::ref_ptr<IShellFolder> folder(iFolder, false);
+				IContextMenu *iMenu;
+
+				if (SUCCEEDED(res = folder->GetUIObjectOf(NULL, storage.size(), storage.data(), IID_IContextMenu, NULL, (void**)&iMenu)))
+				{					m_menu0.set(iMenu, false);
+
+					if (SUCCEEDED(m_menu0->QueryInterface(IID_IContextMenu2, (void**)&iMenu)))
+						m_menu2.set(static_cast<IContextMenu2*>(iMenu), false);
+
+					if (SUCCEEDED(m_menu0->QueryInterface(IID_IContextMenu3, (void**)&iMenu)))
+						m_menu3.set(static_cast<IContextMenu3*>(iMenu), false);
+
+					if (HMENU hmenu = CreatePopupMenu())
+					{
+						if (SUCCEEDED(m_menu0->QueryContextMenu(hmenu, 0, 0, 1024, CMF_NORMAL)))
+						{
+							populateMenu(hmenu);
+							invokeCommand(m_commands.value(m_menu.exec(pos)));
+						}
+//							{
+//								oldFilter = Application::instance()->setWinEventFilter(eventFilter);
+//								TrackPopupMenuEx(hmenu, TPM_NONOTIFY | TPM_RETURNCMD | TPM_LEFTBUTTON, pos.x(), pos.y(), parent()->winId(), NULL);
+//								Application::instance()->setWinEventFilter(oldFilter);
+//							}
+
+						DestroyMenu(hmenu);
+					}
+				}
+				else
+					QMessageBox::critical(parent(), QString(), qt_error_string(res));
+			}
+
+			CoTaskMemFree(pidl);
+		}
+	}
+}
+
+void ContextMenuWin::populateMenu(HMENU hmenu)
+{
+	char buffer[BufferSize];
+	MENUITEMINFO info;
+
+	memset(&info, 0, sizeof(MENUITEMINFO));
+	info.cbSize = sizeof(MENUITEMINFO);
+	info.dwTypeData = (wchar_t*)buffer;
+
+	m_menu.clear();
+	m_commands.clear();
+	populateMenu(&m_menu, hmenu, info, buffer);
+}
+
+void ContextMenuWin::populateMenu(QMenu *menu, HMENU hmenu, MENUITEMINFO &info, char *buffer)
+{
+	QIcon icon;
+	QString title;
+
+	for (int i = 0, size = GetMenuItemCount(hmenu); i < size; ++i)
+	{
+		info.fMask = MIIM_FTYPE;
+
+		if (GetMenuItemInfo(hmenu, i, true, &info))
+			if ((info.fType & MFT_SEPARATOR) == MFT_SEPARATOR)
+				menu->addSeparator();
+			else
+				if ((info.fType & MFT_STRING) == MFT_STRING)
+				{
+					memset(buffer, 0, BufferSize);
+					info.fMask = MIIM_ID | MIIM_STRING | MIIM_BITMAP | MIIM_SUBMENU | MIIM_CHECKMARKS;
+					info.cch = BufferSize;
+
+					if (GetMenuItemInfo(hmenu, i, true, &info))
+						title = QString::fromUtf16((const ushort*)info.dwTypeData, info.cch);
+
+					if (info.hbmpItem != 0 &&
+						info.hbmpItem != HBMMENU_CALLBACK &&
+						info.hbmpItem != HBMMENU_MBAR_CLOSE &&
+						info.hbmpItem != HBMMENU_MBAR_CLOSE_D &&
+						info.hbmpItem != HBMMENU_MBAR_MINIMIZE &&
+						info.hbmpItem != HBMMENU_MBAR_MINIMIZE_D &&
+						info.hbmpItem != HBMMENU_MBAR_RESTORE &&
+						info.hbmpItem != HBMMENU_POPUP_CLOSE &&
+						info.hbmpItem != HBMMENU_POPUP_MAXIMIZE &&
+						info.hbmpItem != HBMMENU_POPUP_MINIMIZE &&
+						info.hbmpItem != HBMMENU_POPUP_RESTORE &&
+						info.hbmpItem != HBMMENU_SYSTEM)
+						icon = QIcon(QPixmap::fromWinHBITMAP((HBITMAP)info.hbmpItem));
+					else
+						if (info.hbmpChecked != 0)
+							icon = QIcon(QPixmap::fromWinHBITMAP((HBITMAP)info.hbmpChecked));
+						else
+							if (info.hbmpUnchecked != 0)
+								icon = QIcon(QPixmap::fromWinHBITMAP((HBITMAP)info.hbmpUnchecked));
+							else
+								icon = QIcon();
+
+					if (info.hSubMenu != 0)
+						populateMenu(menu->addMenu(icon, title), info.hSubMenu, info, buffer);
+					else
+					{
+						memset(buffer, 0, BufferSize);
+
+						if (SUCCEEDED(m_menu0->GetCommandString(info.wID, GCS_VERBA, NULL, buffer, BufferSize)))
+							m_commands[menu->addAction(icon, title)] = Command(QByteArray(buffer), info.wID);
+						else
+							m_commands[menu->addAction(icon, title)] = Command(QByteArray(), info.wID);
+					}
+				}
+	}
+}
+
+void ContextMenuWin::invokeCommand(const Command &command)
+{
+	if (!command.first.isEmpty())
+	{
+		CMINVOKECOMMANDINFO info;
+
+		memset(&info, 0, sizeof(CMINVOKECOMMANDINFO));
+		info.cbSize = sizeof(CMINVOKECOMMANDINFO);
+		info.fMask = CMIC_MASK_FLAG_NO_UI;
+		info.hwnd = parent()->winId();
+		info.lpVerb = command.first.constData();
+
+		m_menu0->InvokeCommand(&info);
+	}
+	else
+		if (command.second != 0)
+		{
+			CMINVOKECOMMANDINFO info;
+
+			memset(&info, 0, sizeof(CMINVOKECOMMANDINFO));
+			info.cbSize = sizeof(CMINVOKECOMMANDINFO);
+			info.fMask = CMIC_MASK_FLAG_NO_UI;
+			info.hwnd = parent()->winId();
+			info.lpVerb = MAKEINTRESOURCEA(command.second);
+
+			m_menu0->InvokeCommand(&info);
+		}
+}
+
+//bool ContextMenuWin::eventFilter(MSG *msg, long *result)
+//{
+//	switch (msg->message)
+//	{
+//		case WM_INITMENUPOPUP:
+//		case WM_DRAWITEM:
+//		case WM_MENUCHAR:
+//		case WM_MEASUREITEM:
+//		{
+//			if (!instance->m_menu3.empty())
+//			{
+//				instance->m_menu3->HandleMenuMsg2(msg->message, msg->wParam, msg->lParam, result);
+//				return true;
+//			}
+//
+//			if (!instance->m_menu2.empty())
+//			{
+//				*result = 0;
+//				instance->m_menu2->HandleMenuMsg(msg->message, msg->wParam, msg->lParam);
+//				return true;
+//			}
+//
+//			break;
+//		}
+//		default:
+//			break;
+//	};
+//
+//	return oldFilter ? (*oldFilter)(msg, result) : false;
+//}
--- src/mainwindow/foldersview/contextmenu/win32/contextmenu_win.h
+++ src/mainwindow/foldersview/contextmenu/win32/contextmenu_win.h
@@ -1,1 +1,97 @@
-Unexpected error.  File contents could not be restored from local history during undo/redo.
+#ifndef CONTEXTMENU_WIN_H_
+#define CONTEXTMENU_WIN_H_
+
+#include <QtCore/qt_windows.h>
+#include <QtCore/QByteArray>
+#include <QtCore/QMap>
+#include <QtGui/QMenu>
+#include "../contextmenu.h"
+
+#include <stlsoft/stlsoft.h>
+#if defined(STLSOFT_CF_EXCEPTION_SUPPORT)
+#	undef STLSOFT_CF_EXCEPTION_SUPPORT
+#endif
+#include <stlsoft/smartptr/ref_ptr.hpp>
+#include <comstl/util/initialisers.hpp>
+#include <comstl/util/creation_functions.hpp>
+#include <comstl/util/interface_traits.hpp>
+#include <comstl/util/variant.hpp>
+#include <comstl/string/bstr.hpp>
+#include <ShlObj.h>
+
+
+namespace stlsoft
+{
+	namespace comstl_project
+	{
+		COMSTL_IID_TRAITS_DEFINE(IShellFolder)
+		COMSTL_IID_TRAITS_DEFINE(IContextMenu)
+		COMSTL_IID_TRAITS_DEFINE(IContextMenu2)
+		COMSTL_IID_TRAITS_DEFINE(IContextMenu3)
+	}
+}
+
+
+/* TODO: Menu icons is not drawn for all elements (Tortoise SVN) */
+class ContextMenuWin : public ContextMenu::Implementation
+{
+public:
+	ContextMenuWin(QWidget *parent);
+
+	virtual void popup(const QString &parentDir, const QStringList &files, const QPoint &pos);
+
+private:
+	class ItemIdListStorage
+	{
+	public:
+		typedef const ITEMIDLIST * value_type;
+		typedef value_type       * storage_type;
+		typedef int                size_type;
+		enum { Bof = (size_type)-1 };
+
+	public:
+		ItemIdListStorage(size_type size) :
+			m_data(static_cast<storage_type>(CoTaskMemAlloc(size * sizeof(value_type)))),
+			m_index(Bof)
+		{
+			Q_ASSERT(size > 0);
+		}
+		~ItemIdListStorage()
+		{
+			for (size_type i = 0, sz = size(); i < sz; ++i)
+				CoTaskMemFree((void*)m_data[i]);
+
+			CoTaskMemFree(m_data);
+		}
+
+		inline size_type size() const { return m_index + 1; }
+		inline bool isEmpty() const { return m_index == Bof; }
+		inline void push_back(value_type value) { m_data[++m_index] = value; }
+	    inline storage_type data() { return m_data; }
+
+	private:
+	    storage_type m_data;
+	    size_type m_index;
+	};
+
+private:
+    typedef QPair<QByteArray, UINT> Command;
+    enum { BufferSize = 256 };
+    void populateMenu(HMENU hmenu);
+    void populateMenu(QMenu *menu, HMENU hmenu, MENUITEMINFO &info, char *buffer);
+    void invokeCommand(const Command &command);
+
+//private:
+//    static bool eventFilter(MSG *message, long *result);
+
+private:
+	QMenu m_menu;
+	comstl::com_initialiser m_init;
+	stlsoft::ref_ptr<IShellFolder> m_desktop;
+	stlsoft::ref_ptr<IContextMenu> m_menu0;
+	stlsoft::ref_ptr<IContextMenu2> m_menu2;
+	stlsoft::ref_ptr<IContextMenu3> m_menu3;
+	QMap<QAction*, Command> m_commands;
+};
+
+#endif /* CONTEXTMENU_WIN_H_ */

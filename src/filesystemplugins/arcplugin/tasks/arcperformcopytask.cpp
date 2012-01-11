ee3a34eb7a7de5d89f107c1e534441e4a7b5e4af
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-01-11 11:16:11 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@526 18ec6afb-3d25-4488-acc2-f73458515831

--- src/filesystemplugins/arcplugin/tasks/arcperformcopytask.cpp
+++ src/filesystemplugins/arcplugin/tasks/arcperformcopytask.cpp
@@ -3,9 +3,12 @@
 
 ARC_PLUGIN_NS_BEGIN
 
-PerformCopyTask::PerformCopyTask(const QString &fileName, TasksNode *receiver) :
+PerformCopyTask::PerformCopyTask(const QString &fileName, const ArcNodeItem * item, PScopedPointer<ICopyControl> &control, bool move, TasksNode *receiver) :
 	BaseTask(receiver),
-	m_fileName(fileName)
+	m_fileName(fileName),
+	m_item(item),
+	m_control(control.take()),
+	m_move(move)
 {}
 
 void PerformCopyTask::run(const volatile bool &aborted)
--- src/filesystemplugins/arcplugin/tasks/arcperformcopytask.h
+++ src/filesystemplugins/arcplugin/tasks/arcperformcopytask.h
@@ -2,6 +2,9 @@
 #define ARCPERFORMCOPYTASK_H_
 
 #include "arctaskevent.h"
+#include "../nodes/items/arcnodeitem.h"
+#include "../../../tools/pointers/pscopedpointer.h"
+#include "../../../filesystem/tasks/interfaces/filesystemicopycontrol.h"
 
 
 ARC_PLUGIN_NS_BEGIN
@@ -12,19 +15,32 @@ public:
 	class Event : public TaskEvent
 	{
 	public:
-		Event() :
-			TaskEvent(CopyComplete)
+		Event(BaseTask *task, PScopedPointer<ICopyControl> &control, bool move) :
+			TaskEvent(CopyComplete),
+			task(task),
+			control(control.take()),
+			canceled(canceled),
+			move(move)
 		{}
+
+		BaseTask *task;
+		const ArcNodeItem * item;
+		PScopedPointer<ICopyControl> control;
+		bool canceled;
+		bool move;
 	};
 
 public:
-	PerformCopyTask(const QString &fileName, TasksNode *receiver);
+	PerformCopyTask(const QString &fileName, const ArcNodeItem * item, PScopedPointer<ICopyControl> &control, bool move, TasksNode *receiver);
 
 protected:
 	virtual void run(const volatile bool &aborted);
 
 private:
 	QString m_fileName;
+	const ArcNodeItem * m_item;
+	PScopedPointer<ICopyControl> m_control;
+	bool m_move;
 };
 
 ARC_PLUGIN_NS_END

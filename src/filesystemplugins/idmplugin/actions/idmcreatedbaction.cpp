03d275e4af1bd97b3db23cf288eb507eb3b81cb3
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-03-05 15:52:07 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@618 18ec6afb-3d25-4488-acc2-f73458515831

--- src/filesystemplugins/idmplugin/actions/idmcreatedbaction.cpp
+++ src/filesystemplugins/idmplugin/actions/idmcreatedbaction.cpp
@@ -18,18 +18,37 @@ void CreateDbAction::process(const IFileContainer *container, const FilesList &f
 	IFileContainer::Holder folder;
 
 	for (FilesList::size_type i = 0, size = files.size(); i < size; ++i)
-		if (files.at(i).second->isDir() && (folder = container->open(files.at(i).second->fileName(), false, error)))
-			if (!folder->contains(Plugin::fileName()))
-			{
-				IdmStorage storage(folder->location(Plugin::fileName()), true);
-
-				if (!storage.isValid())
-					QMessageBox::critical(
+		if (files.at(i).second->isDir())
+			if (folder = container->open(files.at(i).second->fileName(), false, error))
+				if (folder->contains(Plugin::fileName()))
+					QMessageBox::warning(
 							Application::mainWindow(),
 							tr("Create database failed!"),
-							tr("Error: ").
-								append(storage.lastError()));
-			}
+							tr("Error: Database already exist in\n"
+							   "\"%1\"").
+								arg(folder->location()));
+				else
+				{
+					IdmStorage storage(folder->location(Plugin::fileName()), true);
+
+					if (storage.isValid())
+						QMessageBox::information(
+								Application::mainWindow(),
+								tr("Create database done!"),
+								tr("All done"));
+					else
+						QMessageBox::critical(
+								Application::mainWindow(),
+								tr("Create database failed!"),
+								tr("Error: ").
+									append(storage.lastError()));
+				}
+			else
+				QMessageBox::critical(
+						Application::mainWindow(),
+						tr("Create database failed!"),
+						tr("Error: ").
+							append(error));
 }
 
 IDM_PLUGIN_NS_END

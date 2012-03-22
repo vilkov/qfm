8880fc46fb6b1182a60268e4e9c38323bd231f21
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-03-22 20:08:37 +0000

    git-svn-id: https://qde.svn.sourceforge.net/svnroot/qde/branch/qfm-arch@657 18ec6afb-3d25-4488-acc2-f73458515831

---
+++ src/filesystemplugins/idmplugin/gui/value/list/editable/widgets/editablevaluelistwidget.cpp
@@ -0,0 +1,3 @@
+#include "editablevaluelistwidget.h"
+
+
---
+++ src/filesystemplugins/idmplugin/gui/value/list/editable/widgets/editablevaluelistwidget.h
@@ -0,0 +1,122 @@
+#ifndef EDITABLEVALUELISTWIDGET_H_
+#define EDITABLEVALUELISTWIDGET_H_
+
+#include <QtGui/QTreeView>
+#include "../model/editablevaluelistmodel.h"
+#include "../../../../../containeres/idmcontainer.h"
+#include "../../../../../storage/queries/idmselectquery.h"
+#include "../../../../../../../tools/events/imp/keyboardeventhandler.h"
+#include "../../../../../../../tools/events/imp/keyboardeventsource.h"
+#include "../../../../../../../tools/widgets/nestedplaindialog/widgets/nestedwidget.h"
+
+
+using namespace FileSystem::Plugins::Idm;
+
+class EditableValueListWidgetPrivate
+{
+public:
+	class ICallback
+	{
+	public:
+		virtual ~ICallback();
+
+		virtual NestedDialog *parent() = 0;
+		virtual void critical(const QString &text) = 0;
+	};
+
+	typedef KeyboardEventSource<
+				EventSourceBase<
+					QTreeView
+				>
+			> TreeView;
+
+public:
+	EditableValueListWidgetPrivate(ICallback *callback, EventHandler *handler, const IdmContainer &container, const IdmEntityValue::Holder &value);
+
+	const IdmContainer &container() const { return m_container; }
+	IdmContainer &container() { return m_container; }
+
+	const TreeView &view() const { return m_view; }
+	TreeView &view() { return m_view; }
+
+	const CompositeValueModel &model() const { return m_model; }
+	CompositeValueModel &model() { return m_model; }
+
+	void addValue(const QModelIndex &index);
+	void removeValue(const QModelIndex &index);
+
+private:
+	ICallback *m_callback;
+	IdmContainer m_container;
+	IdmEntityValue::Holder m_value;
+	TreeView m_view;
+	CompositeValueModel m_model;
+};
+
+
+class MainEditableValueListWidget : public BaseNestedWidget, public CompositeValueWidgetPrivate::ICallback
+{
+public:
+	MainEditableValueListWidget(EventHandler *handler, const IdmContainer &container, const IdmEntityValue::Holder &value, NestedDialog *parent);
+
+	/* BaseNestedWidget */
+	virtual QWidget *centralWidget();
+	virtual void setReadOnly(bool value);
+	virtual void setFocus();
+
+	/* CompositeValueWidgetPrivate::ICallback */
+	virtual NestedDialog *parent();
+	virtual void critical(const QString &text);
+
+    QModelIndex currentIndex() const { return m_private.view().selectionModel()->currentIndex(); }
+
+    const IdmContainer &container() const { return m_private.container(); }
+	IdmContainer &container() { return m_private.container(); }
+
+	const CompositeValueModel &model() const { return m_private.model(); }
+	CompositeValueModel &model() { return m_private.model(); }
+
+	void addValue(const QModelIndex &index) { m_private.addValue(index); }
+	void removeValue(const QModelIndex &index) { m_private.removeValue(index); }
+
+private:
+	CompositeValueWidgetPrivate m_private;
+};
+
+
+class EditableValueListWidget : public NestedWidget, public CompositeValueWidgetPrivate::ICallback
+{
+public:
+	EditableValueListWidget(const IdmContainer &container, const IdmEntityValue::Holder &value, NestedDialog *parent, const QString &title);
+
+	/* BaseNestedWidget */
+	virtual void setFocus();
+
+	/* CompositeValueWidgetPrivate::ICallback */
+	virtual NestedDialog *parent();
+	virtual void critical(const QString &text);
+
+    QModelIndex currentIndex() const { return m_private.view().selectionModel()->currentIndex(); }
+
+    const IdmContainer &container() const { return m_private.container(); }
+	IdmContainer &container() { return m_private.container(); }
+
+	const CompositeValueModel &model() const { return m_private.model(); }
+	CompositeValueModel &model() { return m_private.model(); }
+
+	void addValue();
+	void removeValue();
+
+private:
+	typedef KeyboardEventHandler<
+				EventHandlerBase<
+					CompositeValueWidget
+				>
+			> TreeViewHandler;
+
+private:
+	TreeViewHandler m_handler;
+	CompositeValueWidgetPrivate m_private;
+};
+
+#endif /* EDITABLEVALUELISTWIDGET_H_ */

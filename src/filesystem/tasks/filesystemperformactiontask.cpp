d1b52b2100628314dd9236b22cb386a7a847069f
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-04-24 23:28:41 +0400

    Move PerformActionTask into filesystem.

---
+++ src/filesystem/tasks/filesystemperformactiontask.cpp
@@ -0,0 +1,17 @@
+#include "filesystemperformactiontask.h"
+
+
+FILE_SYSTEM_NS_BEGIN
+
+PerformActionTask::PerformActionTask(TasksNode *receiver, const AsyncFileAction::FilesList &files) :
+	BaseTask(receiver),
+	m_files(files)
+{}
+
+void PerformActionTask::run(const volatile Flags &aborted)
+{
+	process(aborted);
+	postEvent(new Event(this, Event::Action, m_files, aborted));
+}
+
+FILE_SYSTEM_NS_END
---
+++ src/filesystem/tasks/filesystemperformactiontask.h
@@ -0,0 +1,44 @@
+#ifndef FILESYSTEMPERFORMACTIONTASK_H_
+#define FILESYSTEMPERFORMACTIONTASK_H_
+
+#include "filesystembasetask.h"
+#include "tools/taskprogress.h"
+#include "../actions/asynchronous/filesystemasyncfileaction.h"
+
+
+FILE_SYSTEM_NS_BEGIN
+
+class PerformActionTask : public BaseTask
+{
+public:
+	class Event : public BaseTask::Event
+	{
+	public:
+		typedef BaseTask::Event::Type Type;
+
+	public:
+		Event(BaseTask *task, Type type, const AsyncFileAction::FilesList &files, bool canceled) :
+			BaseTask::Event(task, static_cast<Type>(type), canceled),
+			files(files)
+		{}
+
+		AsyncFileAction::FilesList files;
+	};
+
+public:
+	PerformActionTask(TasksNode *receiver, const AsyncFileAction::FilesList &files);
+
+protected:
+	virtual void run(const volatile Flags &aborted);
+	virtual void process(const volatile Flags &aborted) = 0;
+
+protected:
+	const AsyncFileAction::FilesList &files() const { return m_files; }
+
+private:
+	AsyncFileAction::FilesList m_files;
+};
+
+FILE_SYSTEM_NS_END
+
+#endif /* FILESYSTEMPERFORMACTIONTASK_H_ */

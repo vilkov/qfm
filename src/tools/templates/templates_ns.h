3b166d9552faf3eade5ad5f5051b02914e0833d0
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-05-05 13:53:25 +0400

    Update tools.

---
+++ src/tools/templates/templates_ns.h
@@ -0,0 +1,10 @@
+#ifndef TEMPLATES_NS_H_
+#define TEMPLATES_NS_H_
+
+#include "../tools_ns.h"
+
+
+#define TEMPLATES_NS_BEGIN TOOLS_NS_BEGIN namespace Templates {
+#define TEMPLATES_NS_END } TOOLS_NS_END
+
+#endif /* TEMPLATES_NS_H_ */

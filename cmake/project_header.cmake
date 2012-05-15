7b0440817c0faf8e1155e33a0fb4500d0594038a
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-05-15 14:45:18 +0400

    Add project_header.cmake.

---
+++ cmake/project_header.cmake
@@ -0,0 +1,38 @@
+# Project header
+if (CMAKE_CROSSCOMPILING)
+    message (STATUS "Cross compiling to: " ${CMAKE_SYSTEM_NAME} " " ${CMAKE_SYSTEM_VERSION})
+else ()
+    message (STATUS "System: " ${CMAKE_SYSTEM_NAME} " " ${CMAKE_SYSTEM_VERSION})
+endif ()
+message (STATUS "Processor: " ${CMAKE_HOST_SYSTEM_PROCESSOR})
+if (MSVC)
+	message(STATUS "Compiler: MSVC, version: " ${MSVC_VERSION})
+
+	# C CXX Flags - turn on WARNINGS
+    set (PROJECT_COMP_FLAGS "/W3")
+    
+elseif (BORLAND)
+	message(STATUS "Compiler: BCC")
+elseif (CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUC) 
+	message(STATUS "Compiler: GCC")
+	
+	# C CXX Flags - turn on WARNINGS
+    set (PROJECT_COMP_FLAGS "-Wall -Wextra -pedantic -Wundef -Wcast-align -Wpointer-arith -Wformat")
+
+	# C CXX Flags - turn off WARNINGS
+    set (PROJECT_COMP_FLAGS "${PROJECT_COMP_FLAGS} -Wno-unused-parameter -Wno-parentheses -Wno-shadow -Wno-overloaded-virtual -Wno-ignored-qualifiers")
+	
+	# C CXX Flags - WARNINGS to ERRORS
+    set (PROJECT_COMP_FLAGS "${PROJECT_COMP_FLAGS} -Werror=return-type -Werror=non-virtual-dtor")
+    
+	# C CXX Flags - turn on CODE GEN
+    set (PROJECT_COMP_FLAGS "${PROJECT_COMP_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
+    
+	# C CXX Flags - turn off CODE GEN
+    set (PROJECT_COMP_FLAGS "${PROJECT_COMP_FLAGS} -fno-exceptions -fno-check-new -fno-common -fno-threadsafe-statics")
+
+    #Useful link: "http://gcc.gnu.org/onlinedocs/gcc/Invoking-GCC.html"
+else () 
+	message (STATUS "Unknown compiler")
+endif ()
+message (STATUS "CMake generates " ${CMAKE_GENERATOR})

ad780f43c5190b5bb4c73c1e48af89013750e250
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-05-16 19:56:45 +0400

    Add "project_build_type". Update "CMakeLists.txt" files.

--- cmake/project_cxx_header.cmake
+++ cmake/project_cxx_header.cmake
@@ -1,3 +1,17 @@
+# Copyright (C) 2012  Dmitriy Vilkov <dav.daemon@gmail.com>
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file COPYING-CMAKE-SCRIPTS for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+
+
+# Set up BUILD_TYPE variables
+include (project_build_type)
+
+
 # Project header
 if (CMAKE_CROSSCOMPILING)
     message (STATUS "Cross compiling to: " ${CMAKE_SYSTEM_NAME} " " ${CMAKE_SYSTEM_VERSION})
@@ -8,7 +22,7 @@ message (STATUS "Processor: " ${CMAKE_HOST_SYSTEM_PROCESSOR})
 if (MSVC)
 	message(STATUS "Compiler: MSVC, version: " ${MSVC_VERSION})
 
-	# C CXX Flags - turn on WARNINGS
+	# CXX Flags - turn on WARNINGS
     set (PROJECT_COMP_FLAGS "/W3")
     
 elseif (BORLAND)
@@ -16,19 +30,19 @@ elseif (BORLAND)
 elseif (CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUC) 
 	message(STATUS "Compiler: GCC")
 	
-	# C CXX Flags - turn on WARNINGS
+	# CXX Flags - turn on WARNINGS
     set (PROJECT_COMP_FLAGS "-Wall -Wextra -pedantic -Wundef -Wcast-align -Wpointer-arith -Wformat")
 
-	# C CXX Flags - turn off WARNINGS
+	# CXX Flags - turn off WARNINGS
     set (PROJECT_COMP_FLAGS "${PROJECT_COMP_FLAGS} -Wno-unused-parameter -Wno-parentheses -Wno-shadow -Wno-overloaded-virtual -Wno-ignored-qualifiers")
 	
-	# C CXX Flags - WARNINGS to ERRORS
+	# CXX Flags - WARNINGS to ERRORS
     set (PROJECT_COMP_FLAGS "${PROJECT_COMP_FLAGS} -Werror=return-type -Werror=non-virtual-dtor")
     
-	# C CXX Flags - turn on CODE GEN
+	# CXX Flags - turn on CODE GEN
     set (PROJECT_COMP_FLAGS "${PROJECT_COMP_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
     
-	# C CXX Flags - turn off CODE GEN
+	# CXX Flags - turn off CODE GEN
     set (PROJECT_COMP_FLAGS "${PROJECT_COMP_FLAGS} -fno-exceptions -fno-check-new -fno-common -fno-threadsafe-statics")
 
     #Useful link: "http://gcc.gnu.org/onlinedocs/gcc/Invoking-GCC.html"

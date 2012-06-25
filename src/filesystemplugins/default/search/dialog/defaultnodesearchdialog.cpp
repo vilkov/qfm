0a6d7e505bc3dd6ee2432c6c995c76cb4e034d82
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-06-26 01:50:22 +0400

    Add widgets to Plugins::Default::SearchDialog.

--- src/filesystemplugins/default/search/dialog/defaultnodesearchdialog.cpp
+++ src/filesystemplugins/default/search/dialog/defaultnodesearchdialog.cpp
@@ -4,9 +4,36 @@
 DEFAULT_PLUGIN_NS_BEGIN
 
 SearchDialog::SearchDialog(QWidget *parent) :
-	QDialog(parent)
+	QDialog(parent),
+	m_layout(this),
+	m_label(tr("File name pattern"), this),
+	m_editor(this),
+	m_buttonBox(QDialogButtonBox::Cancel | QDialogButtonBox::Ok, Qt::Horizontal, this)
 {
+	m_hLayout.setSpacing(1);
+	m_hLayout.setMargin(1);
 
+	m_hLayout.addWidget(&m_label);
+	m_hLayout.addWidget(&m_editor);
+
+	m_layout.setSpacing(1);
+	m_layout.setMargin(1);
+
+	m_layout.addLayout(&m_hLayout);
+	m_layout.addWidget(&m_buttonBox);
+
+    connect(&m_buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+    connect(&m_buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+}
+
+void SearchDialog::accept()
+{
+	QDialog::accept();
+}
+
+void SearchDialog::reject()
+{
+	QDialog::reject();
 }
 
 DEFAULT_PLUGIN_NS_END
--- src/filesystemplugins/default/search/dialog/defaultnodesearchdialog.h
+++ src/filesystemplugins/default/search/dialog/defaultnodesearchdialog.h
@@ -1,7 +1,12 @@
 #ifndef DEFAULTNODESEARCHDIALOG_H_
 #define DEFAULTNODESEARCHDIALOG_H_
 
+#include <QtGui/QLabel>
 #include <QtGui/QDialog>
+#include <QtGui/QLineEdit>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QDialogButtonBox>
 #include "../../default_ns.h"
 
 
@@ -11,6 +16,16 @@ class SearchDialog : public QDialog
 {
 public:
 	SearchDialog(QWidget *parent = 0);
+
+    virtual void accept();
+    virtual void reject();
+
+private:
+	QVBoxLayout m_layout;
+	QHBoxLayout m_hLayout;
+	QLabel m_label;
+	QLineEdit m_editor;
+	QDialogButtonBox m_buttonBox;
 };
 
 DEFAULT_PLUGIN_NS_END

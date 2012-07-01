69a3235d24282c827ce82e1c8db81076ec2f125d
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-07-01 16:03:19 +0400

    Add implementation of (IFileContainer::Filter) common filters.

---
+++ src/filesystem/filters/filesystemfilters.cpp
@@ -0,0 +1,30 @@
+#include "filesystemfilters.h"
+
+
+FILE_SYSTEM_NS_BEGIN
+
+Filters::~Filters()
+{
+	qDeleteAll(m_filters);
+}
+
+bool Filters::match(const IFileInfo *info) const
+{
+	for (Container::const_iterator i = m_filters.constBegin(), end = m_filters.constEnd(); i != end; ++i)
+		if (!(*i)->match(info))
+			return false;
+
+	return true;
+}
+
+bool FileNameFilter::match(const IFileInfo *info) const
+{
+	return m_pattern.exactMatch(info->fileName());
+}
+
+bool ModificationTimeFilterRange::match(const IFileInfo *info) const
+{
+	return DateFilterRange::match(info->lastModified());
+}
+
+FILE_SYSTEM_NS_END

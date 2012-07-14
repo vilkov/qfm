5e27fafa627fe6d0f76b881f90591dd2adb0f997
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-07-14 13:35:59 +0400

    Remove filesystemplugins.

--- src/filesystemplugins/idmplugin/nodes/query/interface/idminodequeryresults.cpp
+++
@@ -1,9 +0,0 @@
-#include "idminodequeryresults.h"
-
-
-IDM_PLUGIN_NS_BEGIN
-
-IQueryResultsUpdater::~IQueryResultsUpdater()
-{}
-
-IDM_PLUGIN_NS_END
--- src/filesystemplugins/idmplugin/storage/undo/idmstorageundocommand.cpp
+++
@@ -1,9 +0,0 @@
-#include "idmstorageundocommand.h"
-
-
-IDM_PLUGIN_NS_BEGIN
-
-IdmStorageUndoCommand::~IdmStorageUndoCommand()
-{}
-
-IDM_PLUGIN_NS_END

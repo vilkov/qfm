fd3bf018d7082fc1d430ad6cb684996c00d11612
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-08-19 22:55:47 +0400

    Update ::VFS::Plugins::Default::Settings  - add file icon settings.

--- src/vfs-plugins/default/settings/defaultpluginsettings.cpp
+++ src/vfs-plugins/default/settings/defaultpluginsettings.cpp
@@ -2,12 +2,37 @@
 
 
 DEFAULT_PLUGIN_NS_BEGIN
+Settings *s_instance;
+
 
 Settings::Settings(Option *parent) :
 	Page(tr("Default"), QString::fromLatin1("Default"), parent),
-	m_text(tr("This plugin is the default embedded plugin which can handle regular folders and files."))
+	m_text(tr("This plugin is the default embedded plugin which can handle regular folders and files.")),
+	m_fileIcon(this)
 {
+	Q_ASSERT(s_instance == NULL);
+
 	manage(&m_text);
+	manage(&m_fileIcon, &m_fileIcon);
+
+	s_instance = this;
+}
+
+Settings::FileIcon::FileIcon(Option *parent) :
+	GroupBox(tr("File icon"), QString::fromLatin1("FileIcon"), parent),
+	m_onlyAppIcon(tr("Use only icon of application"), QString::fromLatin1("OnlyAppIcon"), this, false),
+	m_onlyTypeIcon(tr("Use only icon of type"), QString::fromLatin1("OnlyTypeIcon"), this, false),
+	m_appIconIfNoTypeIcon(tr("Use icon of application only if there is no icon of type"), QString::fromLatin1("AppIconIfNoTypeIcon"), this, true)
+{
+	manage(&m_onlyAppIcon, &m_onlyAppIcon);
+	manage(&m_onlyTypeIcon, &m_onlyTypeIcon);
+	manage(&m_appIconIfNoTypeIcon, &m_appIconIfNoTypeIcon);
+}
+
+Settings *Settings::instance()
+{
+	Q_ASSERT(s_instance);
+	return s_instance;
 }
 
 DEFAULT_PLUGIN_NS_END
--- src/vfs-plugins/default/settings/defaultpluginsettings.h
+++ src/vfs-plugins/default/settings/defaultpluginsettings.h
@@ -5,6 +5,8 @@
 #include "../default_ns.h"
 #include "../../../tools/settings/options/gui/page/settingspage.h"
 #include "../../../tools/settings/options/gui/decoration/settingsdecorationtext.h"
+#include "../../../tools/settings/options/gui/groupbox/settingsgroupbox.h"
+#include "../../../tools/settings/options/gui/radiobutton/settingsradiobutton.h"
 
 
 DEFAULT_PLUGIN_NS_BEGIN
@@ -14,10 +16,33 @@ class Settings : public ::Tools::Settings::Page
 	Q_DECLARE_TR_FUNCTIONS(Settings)
 
 public:
+	class FileIcon : public ::Tools::Settings::GroupBox
+	{
+		Q_DECLARE_TR_FUNCTIONS(FileIcon)
+
+	public:
+		FileIcon(Option *parent);
+
+		const ::Tools::Settings::RadioButton &onlyAppIcon() const { return m_onlyAppIcon; }
+		const ::Tools::Settings::RadioButton &onlyTypeIcon() const { return m_onlyTypeIcon; }
+		const ::Tools::Settings::RadioButton &appIconIfNoTypeIcon() const { return m_appIconIfNoTypeIcon; }
+
+	private:
+		::Tools::Settings::RadioButton m_onlyAppIcon;
+		::Tools::Settings::RadioButton m_onlyTypeIcon;
+		::Tools::Settings::RadioButton m_appIconIfNoTypeIcon;
+	};
+
+public:
 	Settings(Option *parent);
+
+	static Settings *instance();
+
+	const FileIcon &fileIcon() const { return m_fileIcon; }
 
 private:
 	::Tools::Settings::DecorationText m_text;
+	FileIcon m_fileIcon;
 };
 
 DEFAULT_PLUGIN_NS_END

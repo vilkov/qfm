5ea5b6b0663b6dab3ec55d91c259e1acdf489961
Dmitriy Vilkov <dav.daemon@gmail.com>
2013-08-11 12:26:12 +0400

    Fix build script.

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-export PATH="/home/dav/WORK/workspace/3rdparty/stlsoft-1.9.108/include:${PATH}"
-#export PATH="/media/WORKSPACE/SVN/3rdparty/boost_1_47_0-bin/include:${PATH}"
-#export PATH="/media/WORKSPACE/SVN/3rdparty/boost_1_47_0-bin/lib:${PATH}"
 
-
-# Qt5
-#unset QTDIR
-#export LD_LIBRARY_PATH="/media/BACKUP/qt5/qtbase/lib"
-#export PATH="/media/BACKUP/qt5/qtbase/include:${PATH}"
-#export PATH="/media/BACKUP/qt5/qtbase/lib:${PATH}"
-#export PATH="/media/BACKUP/qt5/qtbase/bin:/media/BACKUP/qt5/qtrepotools/bin:${PATH}"
-
-
-export PATH="/home/dav/xdg/include:${PATH}"
-export PATH="/home/dav/xdg/lib:${PATH}"
-
-cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE:STRING=$1 ../
-make
+# $1 - Debug or Release
+cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE:STRING=$1 ../workspace &&
+make qfm
